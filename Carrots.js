7ce779e2450575b0a581721daaa9bdfa9e6c3159
Ian Pike <Ianpike98@gmail.com>
2016-03-04 12:56:31 -0500

    Update Carrots.js

--- Carrots.js
+++ Carrots.js
@@ -1,90 +1,40 @@
-// JavaScript Document
-var xmlHttp = createXmlHttpRequestObject();
-
-function createXmlHttpRequestObject(){
-	var xmlHttp;
-	
-	if(window.XMLHttpRequest) {
-		xmlHttp = new XMLHttpRequest();	
-	} else {
-		xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+function sendRequest(url,callback,postData) {
+	var req = createXMLHTTPObject();
+	if (!req) return;
+	var method = (postData) ? "POST" : "GET";
+	req.open(method,url,true);
+	req.setRequestHeader('User-Agent','XMLHTTP/1.0');
+	if (postData)
+		req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	req.onreadystatechange = function () {
+		if (req.readyState != 4) return;
+		if (req.status != 200 && req.status != 304) {
+//			alert('HTTP error ' + req.status);
+			return;
+		}
+		callback(req);
 	}
-	return xmlHttp;
+	if (req.readyState == 4) return;
+	req.send(postData);
 }
 
-/*function readTextFile(Carrots.txt)
-{
-	var rawFile = new XMLHttpRequest();
-	rawFile.open("GET",Carrots.txt,true);
-	rawFile.onreadystatechange = function ()
-	{
-		if(rawFile.readyState === 4)
-		{
-			if(rawFile.status === 200 || rawFile.status == 0)
-			{
-				var allText = rawFile.responseText;
-				alert(allText);
-			}
-		}
-	}
-	rawFile.send(#tool);
-}
-*/
-function process() 
-{
-	if(xmlHttp) 
-	{
-		try
-		{
-			xmlHttp.open("GET", "Carrots.txt", true);
-			xmlHttp.onreadystatechange = handleServerResponse;
-			xmlHttp.send(null);
-		}
-		catch(e)
-		{
-		alert.e.toString();
-		}
-	}
-}
+var XMLHttpFactories = [
+	function () {return new XMLHttpRequest()},
+	function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+	function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+	function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+];
 
-function handleServerResponse(Carrots.txt) 
-{
-	var theD = document.getElementById('theD');
-	if(xmlHttp.readyState==1)
-	{
-		theD.innerHTML += "Status 1: server connection established <br/>";	
-	} 
-	
-	else if(xmlHttp.readyState==2)
-	{
-		theD.innerHTML += "Status 2: request received <br/>";	
-	}
-	
-	else if(xmlHttp.readyState==3)
-	{
-		theD.innerHTML += "Status 3: processing request <br/>";	
-	}
-	
-	else if(xmlHttp.readyState==4)
-	{
-		if(xmlHttp.status==200 || xmlHttp.status==0)
-		{
-				try
-				{
-					var text = xmlHttp.responseText;
-					theD.innerHTML += "Status 4: request is finished and response is ready <br/>";
-					theD.innerHTML += text;	
-				}
-				
-				catch(e)
-				{
-				alert(e.toString());	
+function createXMLHTTPObject() {
+	var xmlhttp = false;
+	for (var i=0;i<XMLHttpFactories.length;i++) {
+		try {
+			xmlhttp = XMLHttpFactories[i]();
 		}
-		} 
-		else 
-		{
-			alert(xmlHttp.statusText);
+		catch (e) {
+			continue;
 		}
+		break;
 	}
+	return xmlhttp;
 }
-
